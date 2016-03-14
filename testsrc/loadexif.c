a0d966d3e868dfaaa72de97a6163049f3024ffeb
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-03-14 23:38:23 +0100

    Type fix

--- testsrc/loadexif.c
+++ testsrc/loadexif.c
@@ -14,11 +14,13 @@ void loadexif_setup_overlay(j_decompress_ptr cinfo)
 	struct loadexif_client_data *data = calloc(sizeof (struct loadexif_client_data), 1);
 	loadimage_jpeg_client_data *client_data = (loadimage_jpeg_client_data *)cinfo->client_data;
 	
-	data->orgf.init_source = cinfo->src.init_source;
-	data->orgf.fill_input_buffer = cinfo->src.fill_input_buffer;
-	data->orgf.skip_input_data = cinfo->src.skip_input_data;
-	data->orgf.resync_to_restart = cinfo->src.resync_to_restart;
-	data->orgf.term_source = cinfo->src.term_source;
+	struct jpeg_source_mgr *orgsrc = cinfo->src;
+	
+	data->orgf.init_source = orgsrc->init_source;
+	data->orgf.fill_input_buffer = orgsrc->fill_input_buffer;
+	data->orgf.skip_input_data = orgsrc->skip_input_data;
+	data->orgf.resync_to_restart = orgsrc->resync_to_restart;
+	data->orgf.term_source = orgsrc->term_source;
 	
 	client_data->exif_data = data;
 }

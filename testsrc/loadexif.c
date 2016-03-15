953cc44f4037541e7ff2767811e5738929734e62
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-03-15 16:07:37 +0100

    Bug fixing

--- testsrc/loadexif.c
+++ testsrc/loadexif.c
@@ -11,24 +11,7 @@
 
 #include <jpeglib.h>
 
-void loadexif_setup_overlay(j_decompress_ptr cinfo)
-{
-	struct loadexif_client_data *data = calloc(sizeof (struct loadexif_client_data), 1);
-	loadimage_jpeg_client_data *client_data = (loadimage_jpeg_client_data *)cinfo->client_data;
-	
-	struct jpeg_source_mgr *orgsrc = cinfo->src;
-	
-	data->orgf.init_source = orgsrc->init_source;
-	data->orgf.fill_input_buffer = orgsrc->fill_input_buffer;
-	data->orgf.skip_input_data = orgsrc->skip_input_data;
-	data->orgf.resync_to_restart = orgsrc->resync_to_restart;
-	data->orgf.term_source = orgsrc->term_source;
-	
-	client_data->exif_data = data;
-	
-	orgsrc->skip_input_data = skip_input_data;
-	orgsrc->fill_input_buffer = fill_input_buffer_and_record;
-}
+#incnlude <string.h>
 
 /* Taken from jdatasrc.c; we need this behavior because we need
  * all bytes from the file */
@@ -54,24 +37,46 @@ static void skip_input_data (j_decompress_ptr cinfo, long num_bytes)
 	}
 }
 
-static bool fill_input_buffer_and_record(j_decompress_ptr cinfo)
+static boolean fill_input_buffer_and_record(j_decompress_ptr cinfo)
 {
 	loadimage_jpeg_client_data *client_data = (loadimage_jpeg_client_data *)cinfo->client_data;
 	struct loadexif_client_data *data = client_data->exif_data;
 	
-	bool ret = data->orgf.fill_input_buffer(cinfo);
+	boolean ret = data->orgf.fill_input_buffer(cinfo);
 	
 	if (!ret) return ret;
 	
-	size_t bytes_to_copy = cinfo->bytes_in_buffer;
+	size_t bytes_to_copy = cinfo->src->bytes_in_buffer;
 	if (bytes_to_copy > (LOADEXIF_BUFFER_SIZE - data->inputsize)) {
 		bytes_to_copy = LOADEXIF_BUFFER_SIZE - data->inputsize;
 	}
 	
 	if (bytes_to_copy) {
-		memcpy (data->inputdata + data->inputsize, cinfo->next_input_byte, bytes_to_copy);
+		memcpy (data->inputdata + data->inputsize, cinfo->src->next_input_byte, bytes_to_copy);
 		data->inputsize += bytes_to_copy;
 	}
 	
 	return ret;
 }
+
+
+
+void loadexif_setup_overlay(j_decompress_ptr cinfo)
+{
+	struct loadexif_client_data *data = calloc(sizeof (struct loadexif_client_data), 1);
+	loadimage_jpeg_client_data *client_data = (loadimage_jpeg_client_data *)cinfo->client_data;
+	
+	struct jpeg_source_mgr *orgsrc = cinfo->src;
+	
+	data->orgf.init_source = orgsrc->init_source;
+	data->orgf.fill_input_buffer = orgsrc->fill_input_buffer;
+	data->orgf.skip_input_data = orgsrc->skip_input_data;
+	data->orgf.resync_to_restart = orgsrc->resync_to_restart;
+	data->orgf.term_source = orgsrc->term_source;
+	
+	client_data->exif_data = data;
+	
+	orgsrc->skip_input_data = skip_input_data;
+	orgsrc->fill_input_buffer = fill_input_buffer_and_record;
+}
+

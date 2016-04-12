4cb0fd9c2357be5f9e296dd4fea20887ea44c7e1
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-04-12 18:19:23 +0200

    New gl-image source file

---
+++ testsrc/gl-image.c
@@ -0,0 +1,9 @@
+//
+//  gl-image.c
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 12/04/16.
+//
+//
+
+#include "gl-image.h"
---
+++ testsrc/gl-image.h
@@ -0,0 +1,14 @@
+//
+//  gl-image.h
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 12/04/16.
+//
+//
+
+#ifndef gl_image_h
+#define gl_image_h
+
+#include <stdio.h>
+
+#endif /* gl_image_h */

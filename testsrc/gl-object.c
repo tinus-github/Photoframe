c9a9d66724a89ced15e4ba6ecadbe901988db67c
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-05-03 20:25:31 +0200

    Basic objects

---
+++ testsrc/gl-object.c
@@ -0,0 +1,59 @@
+//
+//  gl-object.c
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 03/05/16.
+//
+// Implements a very basic object scheme
+
+#include "gl-object.h"
+
+#include <assert.h>
+
+static void gl_object_ref(gl_object *obj)
+{
+	atomic_fetch_add(&obj->data.refcount, 1);
+}
+
+static void gl_object_unref(gl_object *obj)
+{
+	unsigned int orgcount = atomic_fetch_sub(&obj->data.refcount, 1);
+	assert (orgcount);
+	if (orgcount == 1) {
+		obj->f->free(obj);
+	}
+}
+
+static void gl_object_free(gl_object *obj)
+{
+	obj->free_fp(obj);
+	free (obj);
+}
+
+static void gl_object_free_fp(gl_object *obj)
+{
+	;
+}
+
+static struct gl_object_funcs gl_object_funcs_global =
+{	.ref = gl_object_ref,
+	.unref = gl_object_unref,
+	.free = gl_object_free,
+	.free_fp = gl_object_free_fp
+};
+
+gl_object *gl_object_init(gl_object *obj)
+{
+	assert (obj->f);
+	obj.f = gl_object_funcs_global;
+	
+	return obj;
+}
+
+gl_object *gl_object_new()
+{
+	gl_object *ret = malloc(sizeof(gl_object));
+	ret->f = malloc(sizeof(gl_object_funcs));
+	
+	return gl_object_init(ret);
+}
---
+++ testsrc/gl-object.h
@@ -0,0 +1,36 @@
+//
+//  gl-object.h
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 03/05/16.
+//
+//
+
+#ifndef gl_object_h
+#define gl_object_h
+
+#include <stdio.h>
+#include <stdatomic.h>
+
+typedef struct gl_object gl_object;
+
+typedef struct gl_object_funcs {
+	void (*free) (gl_object *obj);
+	void (*free_fp) (gl_object *obj);
+	void (*ref) (gl_object *obj);
+	void (*unref) (gl_object *obj);
+} gl_object_funcs;
+
+typedef struct gl_object_data {
+	atomic_uint refcount;
+};
+
+struct gl_object {
+	gl_object_funcs *f;
+	gl_object_data data;
+};
+
+gl_object *gl_object_init(gl_object *obj);
+gl_object *gl_object_new();
+
+#endif /* gl_object_h */

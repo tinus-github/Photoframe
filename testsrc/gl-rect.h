425d059d1a757947d2710993a3ec9d43d5359c03
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-05-15 20:41:47 +0200

    * Include extra file * Makefile fixes

--- testsrc/gl-rect.h
+++ testsrc/gl-rect.h
@@ -9,6 +9,14 @@
 #ifndef gl_rect_h
 #define gl_rect_h
 
+#include "gl-object.h"
+
+typedef struct gl_rect gl_rect;
+
+typedef struct gl_rect_funcs {
+	void (*dummy) (gl_rect obj);
+} gl_rect_funcs;
+
 #include <stdio.h>
 
 #endif /* gl_rect_h */

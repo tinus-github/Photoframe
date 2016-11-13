eef1d42769d6a7417e2b612d730456f16db41047
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-11-14 00:02:17 +0100

    Pointer magic

--- testsrc/gl-shape.c
+++ testsrc/gl-shape.c
@@ -34,7 +34,7 @@ static void gl_shape_draw(gl_shape *obj)
 static void gl_shape_set_projection(gl_shape *obj, mat4x4 new_projection)
 {
 	mat4x4_dup(obj->data.projection, new_projection);
-	obj->f.set_computed_projection_dirty(obj);
+	obj->f->set_computed_projection_dirty(obj);
 }
 
 static void gl_shape_set_computed_projection_dirty(gl_shape *obj)

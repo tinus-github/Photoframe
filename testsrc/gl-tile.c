c495b22785cc99c2fccb90a92d8c424874a23aa4
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-12-04 21:20:35 +0100

    Release texture on freeing tile

--- testsrc/gl-tile.c
+++ testsrc/gl-tile.c
@@ -14,6 +14,8 @@ static struct gl_tile_funcs gl_tile_funcs_global = {
 	.set_texture = &gl_tile_set_texture,
 };
 
+static gl_shape *gl_tile_obj_parent;
+
 // Takes over the reference that was held by the caller
 // You may also send NULL to remove the texture
 static void gl_tile_set_texture(gl_tile *obj, gl_texture *texture)
@@ -29,12 +31,22 @@ static void gl_tile_set_texture(gl_tile *obj, gl_texture *texture)
 	obj->data.texture = texture;
 }
 
+static void gl_tile_free(gl_object *tile_obj)
+{
+	gl_tile *tile = (gl_tile *)tile_obj;
+	
+	gl_tile->f->set_texture(NULL);
+	
+	gl_object *parent_obj = (gl_object *)gl_tile_obj_parent;
+	parent_obj->f->free(tile_obj);
+}
+
 void gl_tile_setup()
 {
 	gl_shape *parent = gl_shape_new();
 	memcpy(&gl_tile_funcs_global.p, parent->f, sizeof(gl_object_funcs));
-	gl_object *parent_obj = (gl_object *)parent;
-	parent_obj->f->free((gl_object *)parent);
+	
+	gl_tile_obj_parent = parent;
 }
 
 gl_tile *gl_tile_init(gl_tile *obj)

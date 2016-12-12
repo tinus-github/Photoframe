411ebdb39c7e5c7b332645247e1e928dbf430d86
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-12-12 15:19:06 +0100

    Load shaders Dependencies

--- testsrc/gl-tile.c
+++ testsrc/gl-tile.c
@@ -7,6 +7,7 @@
 //
 
 #include "gl-tile.h"
+#include "gl-display.h"
 
 static void gl_tile_set_texture(gl_tile *obj, gl_texture *texture);
 
@@ -15,6 +16,55 @@ static struct gl_tile_funcs gl_tile_funcs_global = {
 };
 
 static gl_shape *gl_tile_obj_parent;
+
+static GLuint gl_tile_programObject;
+static GLint
+		gl_tile_programPositionLoc,
+		gl_tile_programTexCoordLoc,
+		gl_tile_programProjectionLoc,
+		gl_tile_programModelViewLoc,
+		gl_tile_programSamplerLoc;
+
+static int gl_tile_load_program() {
+	GLchar vShaderStr[] =
+	"attribute vec4 a_position;            \n"
+	"attribute vec2 a_texCoord;            \n"
+	"uniform mat4 u_projection;            \n"
+	"uniform mat4 u_modelView;             \n"
+	"varying vec2 v_texCoord;              \n"
+	"void main()                           \n"
+	"{                                     \n"
+	"   vec4 p = u_modelView * a_position; \n"
+	"   gl_Position = u_projection * p;    \n"
+	"   v_texCoord = a_texCoord;           \n"
+	"}                                     \n";
+	
+	GLchar fShaderStr[] =
+	"precision mediump float;                            \n"
+	"varying vec2 v_texCoord;                            \n"
+	"uniform sampler2D s_texture;                        \n"
+	"void main()                                         \n"
+	"{                                                   \n"
+	"  gl_FragColor = texture2D( s_texture, v_texCoord );\n"
+	"}                                                   \n";
+	
+	// Load the shaders and get a linked program object
+	gl_tile_programObject = gl_display_load_program ( vShaderStr, fShaderStr );
+	
+	// Get the attribute locations
+	gl_tile_programPositionLoc = glGetAttribLocation ( gl_tile_programObject, "a_position" );
+	gl_tile_programTexCoordLoc = glGetAttribLocation ( gl_tile_programObject, "a_texCoord" );
+	
+	// Get the uniform locations
+	gl_tile_programProjectionLoc = glGetUniformLocation ( gl_tile_programObject, "u_projection" );
+	gl_tile_programModelViewLoc  = glGetUniformLocation ( gl_tile_programObject, "u_modelView" );
+	
+	// Get the sampler location
+	gl_tile_programSamplerLoc = glGetUniformLocation ( gl_tile_programObject, "s_texture" );
+	
+	return GL_TRUE;
+}
+
 
 // Takes over the reference that was held by the caller
 // You may also send NULL to remove the texture
@@ -50,6 +100,8 @@ void gl_tile_setup()
 	objf->free = &gl_tile_free;
 	
 	gl_tile_obj_parent = parent;
+	
+	gl_tile_load_program();
 }
 
 gl_tile *gl_tile_init(gl_tile *obj)

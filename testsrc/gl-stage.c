c03ffe9e5640e180a16dbd16a07c7eaa529307fd
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-12-13 21:05:05 +0100

    gl-stage object

---
+++ testsrc/gl-stage.c
@@ -0,0 +1,45 @@
+//
+//  gl-stage.c
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 13/12/2016.
+//
+//
+//  The stage contains basic information on the size of the screen and other information required for rendering
+
+#include "gl-stage.h"
+
+static void gl_stage_set_dimensions(gl_stage *obj, uint32_t width, uint32_t height);
+
+static struct gl_stage_funcs gl_stage_funcs_global = {
+	.set_dimensions = &gl_stage_set_dimensions
+};
+
+static void gl_stage_set_dimensions(gl_stage *obj, uint32_t width, uint32_t height)
+{
+	obj->data.width = width;
+	obj->data.height = height;
+}
+
+void gl_stage_setup()
+{
+	gl_object *parent = gl_object_new();
+	memcpy(&gl_stage_funcs_global.p, parent->f, sizeof(gl_object_funcs));
+	parent->f->free(parent);
+}
+
+gl_stage *gl_stage_init(gl_stage *obj)
+{
+	gl_object_init((gl_object *)obj);
+	
+	obj->f = &gl_stage_funcs_global;
+	
+	return obj;
+}
+
+gl_stage *gl_stage_new()
+{
+	gl_stage *ret = malloc(sizeof(gl_stage));
+	
+	return gl_stage_init(ret);
+}
---
+++ testsrc/gl-stage.h
@@ -0,0 +1,38 @@
+//
+//  gl-stage.h
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 13/12/2016.
+//
+//
+
+#ifndef gl_stage_h
+#define gl_stage_h
+
+#include <stdio.h>
+
+typedef struct gl_stage gl_stage;
+
+typedef struct gl_stage_funcs {
+	gl_object_funcs p;
+	void (*set_dimensions) (gl_stage *obj, uint32_t width, uint32_t height);
+} gl_stage_funcs;
+
+typedef struct gl_stage_data {
+	gl_object_data p;
+	
+	uint32_t width;
+	uint32_t height;
+} gl_stage_data;
+
+struct gl_stage {
+	gl_stage_funcs *f;
+	gl_stage_data data;
+};
+
+void gl_stage_setup();
+gl_stage *gl_stage_init(gl_stage *obj);
+gl_stage *gl_stage_new();
+
+
+#endif /* gl_stage_h */

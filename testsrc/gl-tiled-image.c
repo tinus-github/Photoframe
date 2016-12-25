24ab4e8f6a31999be2fbe915799c6c48affe3eb1
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-12-25 16:30:20 +0100

    tiled image object

---
+++ testsrc/gl-tiled-image.c
@@ -0,0 +1,94 @@
+//
+//  gl-tiled-image.c
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 25/12/2016.
+//
+//
+
+#include "gl-tiled-image.h"
+
+static struct gl_tiled_image_funcs gl_tiled_image_funcs_global = {
+	.load_image = &gl_tiled_image_load_image
+};
+
+static void gl_tiled_image_free(gl_object *obj_obj);
+static void gl_tiled_image_load_image (gl_tiled_image *obj, unsigned char *rgba_data,
+				       unsigned int width, unsigned int height,
+				       unsigned int orientation, unsigned int tile_height);
+
+static void (*gl_object_free_org_global) (gl_object *obj);
+
+void gl_tiled_image_setup()
+{
+	gl_container_2d *parent = gl_container_2d_new();
+	memcpy(&gl_tiled_image_funcs_global.p, parent->f, sizeof(gl_container_2d_funcs));
+	parent->f->free(parent);
+	
+	gl_object_funcs *obj_funcs_global = (gl_object_funcs *) gl_tiled_image_funcs_global;
+	gl_object_free_org_global = obj_funcs_global.free;
+	obj_funcs_global.free = &gl_tiled_image_free;
+}
+
+static void gl_tiled_image_free(gl_object *obj_obj)
+{
+	gl_tiled_image *obj = (gl_tiled_image *)obj_obj;
+	if (!obj->data.keep_image_data) {
+		free(obj->data.rgba_data);
+	}
+	
+	gl_object_free_org_global(obj_obj);
+}
+
+static void gl_tiled_image_load_image (gl_tiled_image *obj, unsigned char *rgba_data,
+				       unsigned int width, unsigned int height,
+				       unsigned int orientation, unsigned int tile_height)
+{
+	gl_texture *texture;
+	gl_tile *tile;
+	unsigned int current_y = 0;
+	
+	gl_shape *shape_obj = (gl_shape *)obj;
+	gl_container *container_obj = (gl_container *)obj;
+	gl_shape *shape_tile;
+	
+	shape_obj->data.objectWidth = width;
+	shape_obj->data.objectHeight = height;
+	
+	while (current_y < height) {
+		if ((current_y + tile_height) > height) {
+			tile_height = height - current_y;
+		}
+		
+		texture = gl_texture_new();
+		texture->f->load_image_horizontal_tile(texture, rgba_data,
+						       width, height,
+						       tile_height, current_y);
+		
+		tile = gl_tile_new();
+		tile->f->set_texture(tile, texture);
+		
+		shape_tile = (gl_shape *)tile;
+		shape_tile->data.objectY = current_y;
+		
+		container_obj->f->append_child (container_obj, shape_tile);
+		
+		current_y += tile_height;
+	}
+}
+
+gl_tiled_image *gl_tiled_image_init(gl_tiled_image *obj)
+{
+	gl_container_2d_init((gl_container_2d *)obj);
+	
+	obj->f = &gl_tiled_image_funcs_global;
+	
+	return obj;
+}
+
+gl_tiled_image *gl_tiled_image_new()
+{
+	gl_tiled_image *ret = calloc(1, sizeof(gl_tiled_image));
+	
+	return gl_tiled_image_init(ret);
+}
---
+++ testsrc/gl-tiled-image.h
@@ -0,0 +1,44 @@
+//
+//  gl-tiled-image.h
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 25/12/2016.
+//
+//
+
+#ifndef gl_tiled_image_h
+#define gl_tiled_image_h
+
+#include <stdio.h>
+
+#include "gl-container-2d.h"
+
+typedef struct gl_tiled_image gl_tiled_image;
+
+typedef struct gl_tiled_image_funcs {
+	gl_container_2d_funcs p;
+	void (*load_image) (gl_tiled_image *obj, unsigned char *rgba_data,
+			    unsigned int width, unsigned int height,
+			    unsigned int orientation, unsigned int tile_height);
+} gl_tiled_image_funcs;
+
+typedef struct gl_tiled_image_data {
+	gl_container_2d_data p;
+	
+	unsigned int keep_image_data;
+	
+	unsigned int tile_height;
+	unsigned char *rgba_data;
+} gl_tiled_image_data;
+
+struct gl_tiled_image {
+	gl_tiled_image_funcs *f;
+	gl_tiled_image_data data;
+};
+
+void gl_tiled_image_setup();
+gl_container *gl_tiled_image_init(gl_tiled_image *obj);
+gl_container *gl_tiled_image_new();
+
+
+#endif /* gl_tiled_image_h */

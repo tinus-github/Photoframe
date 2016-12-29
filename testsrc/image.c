59b82ca93f9205e5003a3d9c30b3720220ef0616
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2016-12-29 18:32:04 +0100

    Use egl_display

--- testsrc/image.c
+++ testsrc/image.c
@@ -24,18 +24,13 @@
 #include "gl-stage.h"
 #include "gl-tiled-image.h"
 #include "gl-renderloop.h"
+#include "egl-driver.h"
 
 #include "../lib/linmath/linmath.h"
 
 // from esUtil.h
 #define TRUE 1
 #define FALSE 0
-
-void gl_stage_draw(GL_STATE_T *p_state)
-{
-	gl_renderloop *global_loop = gl_renderloop_get_global_renderloop();
-	global_loop->f->run(global_loop);
-}
 
 int main(int argc, char *argv[])
 {
@@ -47,7 +42,6 @@ int main(int argc, char *argv[])
 	float deltatime;
 	
 	unsigned char *image;
-	GL_STATE_T state, *p_state = &state;
 	
 	//image = esLoadTGA("jan.tga", &width, &height);
 	gettimeofday ( &t1 , &tz );
@@ -68,19 +62,10 @@ int main(int argc, char *argv[])
 	
 	printf("Image loaded in %1.4f seconds\n", deltatime);
 	
-	
-	bcm_host_init();
-	
-	gl_display_init(p_state);
+	egl_driver_setup();
+	egl_driver_init();
 
-#if 0
-	if(!gl_image_init(p_state, image, width, height, orientation))
-		return 0;
 	
-	gl_display_register_draw_func(p_state, gl_image_draw);
-#endif
-
-#if 1
 	gl_tiled_image *tiled_image = gl_tiled_image_new();
 	gl_container_2d *main_container_2d = gl_container_2d_new();
 	gl_container *main_container_2d_container = (gl_container *)main_container_2d;
@@ -89,32 +74,14 @@ int main(int argc, char *argv[])
 	tiled_image->f->load_image(tiled_image, image, width, height, orientation, 128);
 	
 	main_container_2d_container->f->append_child(main_container_2d_container, (gl_shape *)tiled_image);
-	
-//	image_tile = gl_tile_new();
-//	image_tile->f->set_texture(image_tile, image_texture);
-//	main_container_2d_container->f->append_child(main_container_2d_container, (gl_shape *)image_tile);
-//	gl_shape *image_tile_shape = (gl_shape *)image_tile;
-//	image_tile_shape->data.objectX = 150;
-	
-	gl_display_register_draw_func(p_state, gl_stage_draw);
-	
 	main_container_2d_shape->data.objectX = 50.0;
 	main_container_2d->data.scaleH = 2.0;
 	main_container_2d->data.scaleV = 2.0;
 	
 	gl_stage *global_stage = gl_stage_get_global_stage();
 	global_stage->f->set_shape(global_stage, (gl_shape *)main_container_2d);
-#endif
-
-#if 0
-	if (!gl_rect_init(p_state, width, height, 1.0f, 0.0f, 0.0f))
-		return 0;
 	
-	gl_display_register_draw_func(p_state, gl_rect_draw);
-#endif
-	
-	eglSwapBuffers(p_state->display, p_state->surface);
-	esMainLoop(p_state);
+	gl_renderloop_loop();
 	
 	return 0; // not reached
 }

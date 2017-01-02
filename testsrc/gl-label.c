244cc7235ca95b8392e0cb6519308f4304408ea1
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2017-01-03 00:14:54 +0100

    Initial incomplete label object

---
+++ testsrc/gl-label.c
@@ -0,0 +1,76 @@
+//
+//  gl-label.c
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 02/01/2017.
+//
+//
+
+#include "gl-label.h"
+
+#define FONT_FILE "/home/pi/lib/share/font/DejaVuSerif.ttf"
+#define LABEL_HEIGHT 64
+
+void gl_label_free(gl_object *obj);
+
+static struct gl_label_funcs gl_label_funcs_global = {
+	.render = &gl_label_render
+};
+
+struct rendering_data {
+	FT_Library library;
+	FT_Face face;
+	
+};
+
+struct rendering_data global_rendering_data;
+
+static void (*gl_object_free_org_global) (gl_object *obj);
+
+static void gl_label_setup_freetype()
+{
+	FT_Error errorret;
+	assert (!(errorret = FT_Init_Freetype(&global_rendering_data.library)));
+	
+	assert (!(errorret = FT_New_Face(global_rendering_data.library, FONT_FILE, 0, &global_rendering_data.face)));
+
+	assert (!(errorret = FT_Set_Char_Size(global_rendering_data.face, 0, LABEL_HEIGHT*64, 72,72)))
+}
+
+void gl_label_setup()
+{
+	gl_shape *parent = gl_shape_new();
+	memcpy(&gl_label_funcs_global.p, parent->f, sizeof(gl_shape_funcs));
+	
+	gl_object_funcs *obj_funcs_global = (gl_object_funcs *) &gl_label_funcs_global;
+	gl_object_free_org_global = obj_funcs_global->free;
+	obj_funcs_global->free = &gl_label_free;
+	
+	parent->f->free(parent);
+	
+	gl_label_setup_freetype();
+}
+
+gl_label *gl_label_init(gl_label *obj)
+{
+	gl_shape_init((gl_shape *)obj);
+	
+	obj->f = &gl_label_funcs_global;
+	
+	return obj;
+}
+
+gl_label *gl_label_new()
+{
+	gl_label *ret = calloc(1, sizeof(gl_label));
+	
+	return gl_label_init(ret);
+}
+
+void gl_label_free(gl_object *obj_obj)
+{
+	gl_shape *obj = (gl_shape *)obj_obj;
+	free (obj->data.text);
+	
+	gl_object_free_org_global(obj_obj);
+}
---
+++ testsrc/gl-label.h
@@ -0,0 +1,45 @@
+//
+//  gl-label.h
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 02/01/2017.
+//
+//
+
+#ifndef gl_label_h
+#define gl_label_h
+
+#include <stdio.h>
+
+#include "gl-shape.h"
+
+typedef struct gl_label gl_label;
+
+typedef struct gl_label_funcs {
+	gl_shape_funcs p;
+	void (*render) (gl_label *obj);
+} gl_label_funcs;
+
+typedef struct gl_label_data {
+	gl_shape_data p;
+	
+	char *text;
+	uint32_t windowX;
+	uint32_t windowY;
+	uint32_t windowWidth;
+	uint32_t windowHeight;
+	
+	uint32_t usedWidth;
+} gl_label_data;
+
+struct gl_label {
+	gl_label_funcs *f;
+	gl_label_data data;
+};
+
+void gl_label_setup();
+gl_label *gl_label_init(gl_label *obj);
+gl_label *gl_label_new();
+
+
+#endif /* gl_label_h */

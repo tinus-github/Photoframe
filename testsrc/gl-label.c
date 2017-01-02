8fa3fe38ba4c1d94efb87fba564256b72e3e26a1
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2017-01-03 00:21:57 +0100

    * includes * small fixes

--- testsrc/gl-label.c
+++ testsrc/gl-label.c
@@ -9,6 +9,7 @@
 #include "gl-label.h"
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include <assert.h>
 
 #define FONT_FILE "/home/pi/lib/share/font/DejaVuSerif.ttf"
 #define LABEL_HEIGHT 64
@@ -32,23 +33,24 @@ static void (*gl_object_free_org_global) (gl_object *obj);
 static void gl_label_setup_freetype()
 {
 	FT_Error errorret;
-	assert (!(errorret = FT_Init_Freetype(&global_rendering_data.library)));
+	assert (!(errorret = FT_Init_FreeType(&global_rendering_data.library)));
 	
 	assert (!(errorret = FT_New_Face(global_rendering_data.library, FONT_FILE, 0, &global_rendering_data.face)));
 
-	assert (!(errorret = FT_Set_Char_Size(global_rendering_data.face, 0, LABEL_HEIGHT*64, 72,72)))
+	assert (!(errorret = FT_Set_Char_Size(global_rendering_data.face, 0, LABEL_HEIGHT*64, 72,72)));
 }
 
 void gl_label_setup()
 {
 	gl_shape *parent = gl_shape_new();
+	gl_object *parent_obj = (gl_object *)parent;
 	memcpy(&gl_label_funcs_global.p, parent->f, sizeof(gl_shape_funcs));
 	
 	gl_object_funcs *obj_funcs_global = (gl_object_funcs *) &gl_label_funcs_global;
 	gl_object_free_org_global = obj_funcs_global->free;
 	obj_funcs_global->free = &gl_label_free;
 	
-	parent->f->free(parent);
+	parent_obj->f->free(parent_obj);
 	
 	gl_label_setup_freetype();
 }
@@ -71,7 +73,7 @@ gl_label *gl_label_new()
 
 void gl_label_free(gl_object *obj_obj)
 {
-	gl_shape *obj = (gl_shape *)obj_obj;
+	gl_label *obj = (gl_label *)obj_obj;
 	free (obj->data.text);
 	
 	gl_object_free_org_global(obj_obj);

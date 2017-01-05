1cc3819df73aa41ca1251b96e0c5986dc3d7ee56
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2017-01-05 16:03:32 +0100

    label-renderer: similar to gl-label, but better separation of concerns

---
+++ testsrc/gl-label-renderer.c
@@ -0,0 +1,268 @@
+//
+//  gl-label-renderer.c
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 05/01/2017.
+//
+//
+
+#include "gl-label-renderer.h"
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <assert.h>
+#include <string.h>
+#include <hb-ft.h>
+
+#include "gl-object.h"
+#include "gl-tile.h"
+
+#define FONT_FILE "/home/pi/lib/share/font/DejaVuSerif.ttf"
+#define LABEL_HEIGHT 64
+#define LABEL_BASELINE LABEL_HEIGHT
+
+static gl_tile *gl_label_renderer_render(gl_label_renderer *obj,
+					 int32_t windowX, int32_t windowY, int32_t windowWidth, int32_t windowHeight);
+
+static struct gl_label_renderer_funcs gl_label_renderer_funcs_global = {
+	.layout = &gl_label_renderer_layout,
+	.render = &gl_label_renderer_render
+};
+
+struct rendering_data {
+	FT_Library library;
+	FT_Face face;
+	
+	hb_language_t hb_language;
+	hb_font_t *hb_font;
+};
+
+static struct rendering_data global_rendering_data;
+
+typedef struct gl_label_renderer_rect {
+	int x;
+	int y;
+	int width;
+	int height;
+} gl_label_renderer_rect;
+
+static void gl_label_renderer_blit_preclipped(unsigned char* dest, unsigned int dest_stride,
+					      unsigned char* src,  unsigned int src_stride,
+					      unsigned int src_start_x, unsigned int src_end_x,
+					      unsigned int src_start_y, unsigned int src_end_y,
+					      int offset_x, int offset_y)
+{
+	unsigned int counter_y;
+	unsigned int counter_x;
+	int this_dst_x;
+	int this_dst_y;
+	unsigned int this_src_index;
+	unsigned int this_dst_index;
+	
+	for (counter_y = src_start_y; counter_y < src_end_y; counter_y++) {
+		this_dst_y = offset_y + counter_y;
+		for (counter_x = src_start_x; counter_x < src_end_x; counter_x++) {
+			this_dst_x = offset_x + counter_x;
+			this_dst_index = this_dst_x + (dest_stride * this_dst_y);
+			this_src_index = counter_x + (src_stride * counter_y);
+			switch (src[this_src_index]) {
+				case 0:
+					break;
+				case 255:
+					dest[this_dst_index] = 255;
+					break;
+				default:
+					dest[this_dst_index] = src[this_src_index] +
+					(((255 - src[this_src_index]) * dest[this_dst_index]) / 255);
+			}
+		}
+	}
+}
+
+static void gl_label_renderer_blit(unsigned char *dest, gl_label_renderer_rect *dest_rect,
+				   unsigned char *src, gl_label_renderer_rect *src_rect,
+				   int offset_x, int offset_y)
+{
+	int total_offset_x = offset_x;
+	int total_offset_y = offset_y;
+	
+	total_offset_x += src_rect->x;
+	total_offset_y += src_rect->y;
+	total_offset_x -= dest_rect->x;
+	total_offset_y -= dest_rect->y;
+	
+	int blit_start_x = 0;
+	int blit_end_x = src_rect->width;
+	int blit_start_y = 0;
+	int blit_end_y = src_rect->height;
+	
+	if (total_offset_x < 0) {
+		blit_start_x = -total_offset_x;
+	}
+	if (total_offset_y < 0) {
+		blit_start_y = -total_offset_y;
+	}
+	
+	if ((blit_end_x + total_offset_x) > dest_rect->width) {
+		blit_end_x = dest_rect->width - total_offset_x;
+	}
+	if ((blit_end_y + total_offset_y) > dest_rect->height) {
+		blit_end_y = dest_rect->height - total_offset_y;
+	}
+	
+	if ((blit_end_x <= blit_start_x) ||
+	    (blit_end_y <= blit_start_y)) {
+		return;
+	}
+	
+	gl_label_renderer_blit_preclipped(dest, dest_rect->width,
+				 src, src_rect->width,
+				 blit_start_x, blit_end_x,
+				 blit_start_y, blit_end_y,
+				 total_offset_x, total_offset_y);
+}
+
+static void gl_label_renderer_render_character(gl_label_renderer *obj,
+					       uint32_t glyph_index,
+					       int32_t x, int32_t y,
+					       unsigned char* bitmap, gl_label_renderer_rect *bitmap_rect)
+{
+	FT_Error errorret;
+	
+	assert (!(errorret = FT_Load_Glyph(global_rendering_data.face, glyph_index, FT_LOAD_DEFAULT)));
+	
+	FT_GlyphSlot glyph = global_rendering_data.face->glyph;
+	if (glyph->format != FT_GLYPH_FORMAT_BITMAP) {
+		assert (!(errorret = FT_Render_Glyph(glyph, FT_RENDER_MODE_NORMAL)));
+	}
+	
+	gl_label_renderer_rect src_rect_stack;
+	gl_label_renderer_rect *src_rect = &src_rect_stack;
+	src_rect_stack.x = glyph->bitmap_left;
+	src_rect_stack.y = -glyph->bitmap_top;
+	src_rect_stack.width = glyph->bitmap.width;
+	src_rect_stack.height = glyph->bitmap.rows;
+	
+	gl_label_blit(bitmap, bitmap_rect, glyph->bitmap.buffer, src_rect, x, y);
+}
+
+static gt_tile *gl_label_renderer_render(gl_label_renderer *obj,
+					 int32_t windowX, int32_t windowY, int32_t windowWidth, int32_t windowHeight)
+{
+	//gl_label_renderer_layout(obj);
+	
+	unsigned char *bitmap = calloc(1, windowWidth * windowHeight);
+	gl_label_renderer_rect bitmap_rect_stack;
+	gl_label_renderer_rect *bitmap_rect = &bitmap_rect_stack;
+	bitmap_rect->windowX = windowX;
+	bitmap_rect->windowY = windowY;
+	bitmap_rect->windowWidth = windowWidth;
+	bitmap_rect->windowHeight = windowHeight;
+	
+	FT_Face face = global_rendering_data.face;
+	
+	uint32_t counter;
+	uint32_t x_ppem = face->size->metrics.x_ppem;
+	uint32_t maxwidth = ((face->bbox.xMax - face->bbox.xMin) * x_ppem) / face->units_per_EM;
+	
+	int32_t minx = (windowX - maxwidth) * 64;
+	int32_t maxx = (windowX + windowWidth + maxwidth) * 64;
+	for (counter = 0; counter < obj->data.numGlyphs; counter++) {
+		gl_label_glyph_data *glyphdata = &obj->data.glyphData[counter];
+		if (glyphdata->x > maxx) {
+			break;
+		}
+		if (glyphdata->x >= minx ) {
+			gl_label_renderer_render_character(obj,
+							   glyphdata->codepoint,
+							   (glyphdata->x / 64), LABEL_BASELINE + (glyphdata->y / 64),
+							   bitmap, bitmap_rect);
+		}
+	}
+	
+	gl_texture *texture = gl_texture_new();
+	texture->f->load_image_monochrome(texture, bitmap, obj->data.windowWidth, obj->data.windowHeight);
+	
+	texture->data.dataType = gl_texture_data_type_alpha;
+	
+	obj->data.tile = gl_tile_new();
+	gl_tile *tile  = obj->data.tile;
+	tile->f->set_texture(tile, texture);
+	
+	return tile;
+}
+
+static void gl_label_renderer_layout(gl_label_renderer *obj)
+{
+	hb_buffer_t *buf = hb_buffer_create();
+	ssize_t text_length = strlen(obj->data.text);
+	hb_buffer_add_utf8(buf, obj->data.text, text_length, 0, text_length);
+	hb_buffer_set_direction(buf, HB_DIRECTION_LTR);
+	hb_buffer_set_language(buf, global_rendering_data.hb_language);
+	
+	hb_shape(global_rendering_data.hb_font,
+		 buf,
+		 NULL,
+		 0);
+	
+	hb_glyph_info_t *glyph_info = hb_buffer_get_glyph_infos(buf, &obj->data.numGlyphs);
+	hb_glyph_position_t *glyph_pos = hb_buffer_get_glyph_positions(buf, &obj->data.numGlyphs);
+	
+	uint32_t counter;
+	int32_t cursorX = 0;
+	int32_t cursorY = 0;
+	obj->data.glyphData = calloc(obj->data.numGlyphs, sizeof(gl_label_glyph_data));
+	
+	for(counter = 0; counter < obj->data.numGlyphs; counter++) {
+		obj->data.glyphData[counter].x = cursorX + glyph_pos[counter].x_offset;
+		obj->data.glyphData[counter].y = cursorY - glyph_pos[counter].y_offset;
+		cursorX += glyph_pos[counter].x_advance;
+		cursorY += glyph_pos[counter].y_advance;
+		
+		obj->data.glyphData[counter].codepoint = glyph_info[counter].codepoint;
+	}
+	
+	hb_buffer_destroy(buf);
+}
+
+static void gl_label_renderer_setup_freetype()
+{
+	FT_Error errorret;
+	assert (!(errorret = FT_Init_FreeType(&global_rendering_data.library)));
+	
+	assert (!(errorret = FT_New_Face(global_rendering_data.library, FONT_FILE, 0, &global_rendering_data.face)));
+	
+	assert (!(errorret = FT_Set_Char_Size(global_rendering_data.face, 0, LABEL_HEIGHT*64, 72,72)));
+	
+	int counter;
+	for(counter = 0; counter < global_rendering_data.face->num_charmaps; counter++) {
+		if (((global_rendering_data.face->charmaps[counter]->platform_id == 0)
+		     && (global_rendering_data.face->charmaps[counter]->encoding_id == 3))
+		    ||
+		    ((global_rendering_data.face->charmaps[counter]->platform_id == 3)
+		     && (global_rendering_data.face->charmaps[counter]->encoding_id == 1))) {
+			    FT_Set_Charmap(global_rendering_data.face, global_rendering_data.face->charmaps[counter]);
+		    }
+	}
+}
+
+static void gl_label_renderer_setup_harfbuzz()
+{
+	global_rendering_data.hb_language = hb_language_from_string("en", strlen("en"));
+	global_rendering_data.hb_font = hb_ft_font_create(global_rendering_data.face, NULL);
+}
+
+void gl_label_renderer_setup()
+{
+	gl_object *parent = gl_object_new();
+	memcpy(&gl_label_renderer_funcs_global.p, parent->f, sizeof(gl_object_funcs));
+	
+	gl_object_funcs *obj_funcs_global = (gl_object_funcs *) &gl_label_renderer_funcs_global;
+	gl_object_free_org_global = obj_funcs_global->free;
+	obj_funcs_global->free = &gl_label_renderer_free;
+	
+	parent->f->free(parent);
+	
+	gl_label_renderer_setup_freetype();
+	gl_label_renderer_setup_harfbuzz();
+}
---
+++ testsrc/gl-label-renderer.h
@@ -0,0 +1,48 @@
+//
+//  gl-label-renderer.h
+//  Photoframe
+//
+//  Created by Martijn Vernooij on 05/01/2017.
+//
+//
+
+#ifndef gl_label_renderer_h
+#define gl_label_renderer_h
+
+#include <stdio.h>
+
+#include "gl-object.h"
+#include "gl-tile.h"
+
+typedef struct gl_label_renderer gl_label_renderer;
+
+typedef struct gl_label_glyph_data {
+	uint32_t codepoint;
+	int32_t x;
+	int32_t y;
+} gl_label_renderer_glyph_data;
+
+typedef struct gl_label_renderer_funcs {
+	gl_object_funcs p;
+	void (*layout) (gl_label_renderer *obj);
+	gl_tile* (*render) (gl_label_renderer *obj, int32_t windowX, int32_t windowY, int32_t windowWidth, int32_t windowHeight);
+} gl_label_funcs;
+
+typedef struct gl_label_renderer_data {
+	gl_object_data p;
+	
+	char *text; //UTF8
+	uint32_t numGlyphs;
+	gl_label_renderer_glyph_data *glyphData;
+}
+
+struct gl_label_renderer {
+	gl_label_renderer_funcs *f;
+	gl_label_renderer_data data;
+};
+
+void gl_label_renderer_setup();
+gl_label *gl_label_renderer_init(gl_label_renderer *obj);
+gl_label *gl_label_renderer_new();
+
+#endif /* gl_label_renderer_h */

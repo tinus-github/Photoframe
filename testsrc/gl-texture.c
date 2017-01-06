7ec7c13ca83dc3c62ffa1304c07a49f5c9484416
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2017-01-06 22:13:28 +0100

    Set texture wrapping mode to Clamp

--- testsrc/gl-texture.c
+++ testsrc/gl-texture.c
@@ -85,6 +85,10 @@ static GLuint load_image_gen(gl_texture *obj, unsigned char *image_data, unsigne
 	glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
 	glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
 	
+	// Wrapping mode
+	glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	
 	obj->data.textureId = textureId;
 	obj->data.texture_loaded = 1;
 	obj->data.width = width;

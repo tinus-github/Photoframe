6fd19dc82f6690b6a9290bc7c71468f1380d1c31
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2017-01-10 18:07:53 +0100

    Remove debugging statements

--- testsrc/labels/gl-label-scroller-segment.c
+++ testsrc/labels/gl-label-scroller-segment.c
@@ -77,7 +77,6 @@ static void gl_label_scroller_segment_render(gl_label_scroller_segment *obj)
 				found = 1;
 				break;
 			}
-			printf("Discard %d\n", currentChildData->tileIndex);
 			gl_label_scroller_segment_remove_child(obj, currentChildData);
 			currentChildData = nextChildData;
 			nextChildData = currentChildData->siblingR;
@@ -85,7 +84,6 @@ static void gl_label_scroller_segment_render(gl_label_scroller_segment *obj)
 		if (!found) {
 			tile = gl_label_scroller_segment_render_tile(obj, counter);
 			gl_label_scroller_segment_append_child(obj, tile, counter);
-			printf("render %d\n", counter);
 		}
 	}
 }

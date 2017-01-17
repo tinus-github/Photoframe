6b6c8621cc0b5262b1563c2ded5911bfa753da09
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2017-01-17 17:23:36 +0100

    Animation already starts immediately

--- testsrc/image.c
+++ testsrc/image.c
@@ -71,7 +71,6 @@ void fade_in_image(void *target, gl_notice_subscription *sub, void *extra_data)
 	animation->data.action = image_set_alpha;
 	
 	animation->f->start(animation);
-	image_set_alpha(framed_shape, NULL, 0.0);
 }
 
 int main(int argc, char *argv[])

40651375e13bfc8403dad3c0a1c3e21b91ff738a
Martijn Vernooij <tinus@martijn-vernooijs-computer.local>
2017-01-31 22:36:46 +0100

    Smooth horizontal scaling

--- testsrc/images/gl-bitmap-scaler.c
+++ testsrc/images/gl-bitmap-scaler.c
@@ -105,6 +105,8 @@ static void copy_pixel_rgba_to_rgba(unsigned char *outputptr, const unsigned cha
 	outputptr[3] = inputptr[3];
 }
 
+typedef (scale_line_func)(gl_bitmap_scaler *obj, unsigned char *outputptr, const unsigned char *inputptr);
+
 // horizontal scaling funcs
 static void scale_line_coarse(gl_bitmap_scaler *obj, unsigned char *outputptr, const unsigned char *inputptr)
 {
@@ -136,16 +138,99 @@ static void scale_line_coarse(gl_bitmap_scaler *obj, unsigned char *outputptr, c
 	}
 }
 
+static void scale_line_smooth(gl_bitmap_scaler *obj, unsigned char *outputptr, const unsigned char *inputptr)
+{
+	unsigned int current_x_in = 0;
+	unsigned int current_x_out = 0;
+	unsigned int x_total[3];
+	x_total[0] = x_total[1] = x_total[2] = 0;
+	
+	unsigned int remaining_contribution;
+	unsigned int possible_contribution;
+	unsigned int scalerest = 0;
+	unsigned int contribution;
+	
+	unsigned int bytesPerPixel = inputBytesPerPixel(obj);
+	
+	void (* copy_pixel_func) (unsigned char *outputptr, const unsigned char *inputptr);
+	
+	switch (obj->data.inputType) {
+		case gl_bitmap_scaler_input_type_rgb:
+			copy_pixel_func = &copy_pixel_rgb_to_rgba;
+			break;
+		case gl_bitmap_scaler_input_type_rgba:
+			copy_pixel_func = &copy_pixel_rgba_to_rgba;
+			break;
+	}
+	
+	while (current_x_in < obj->data.inputWidth) {
+		remaining_contribution = obj->data.outputWidth;
+		do {
+			possible_contribution = obj->data.inputWidth - scalerest;
+			if (possible_contributino <= remaining_contribution) {
+				contribution = possible_contribution;
+				if (contribution == obj->data.inputWidth) { // if (!scalerest)?
+					copy_pixel_func(outputptr, inputptr);
+				} else {
+					x_total[0] += contribution * inputptr[0];
+					x_total[1] += contribution * inputptr[1];
+					x_total[2] += contribution * inputptr[2];
+					float inputwidthFactor = 1.0 / obj->data.inputWidth;
+					outputptr[0] = x_total[0] * inputWidthFactor;
+					outputptr[1] = x_total[1] * inputWidthFactor;
+					outputptr[2] = x_total[2] * inputWidthFactor;
+					outputptr[3] = 255;
+				}
+				outputptr += 4;
+				current_x_out++;
+				
+				x_total[0] = x_total[1] = x_total[2] = 0;
+				remaining_contribution -= contribution;
+				scalerest = 0;
+				continue;
+			} else {
+				contribution = remaining_contribution;
+				
+				x_total[0] += contribution * inputptr[0];
+				x_total[1] += contribution * inputptr[1];
+				x_total[2] += contribution * inputptr[2];
+				scalerest += remaining_contribution;
+				break;
+			}
+		} while(1);
+		
+		inputptr += bytesPerPixel;
+		current_x_in++;
+	}
+	if (current_x_out < obj->data.outputWidth) {
+		memset(outputptr, 0, 4 * (obj->data.outputWidth - current_x_out));
+	}
+}
+
+static scale_line_func *scale_line_func_for_type(gl_bitmap_scaler_type t)
+{
+	switch (t) {
+		case gl_bitmap_scaler_type_coarse:
+			return &scale_line_coarse;
+		case gl_bitmap_scaler_type_smooth:
+		case gl_bitmap_scaler_type_bresenham:
+			return &scale_line_smooth;
+	}
+	return NULL;
+}
+
 // vertical scaling funcs
 static void add_line_coarse(gl_bitmap_scaler *obj, unsigned char *outputbuf, const unsigned char *inputbuf)
 {
+	scale_line_func *line_func = scale_line_func_for_type(obj->data.verticalType);
+	
 	obj->data._scaleRest += obj->data.outputHeight;
 
 	while (obj->data._scaleRest > obj->data.inputHeight) {
 		const unsigned char *inputptr = inputbuf;
 		unsigned char *outputptr = outputbuf + 4 * obj->data.outputWidth * obj->data._current_y_out;
 		
-		scale_line_coarse(obj, outputptr, inputptr);
+		line_func(obj, outputptr, inputptr);
 		
 		obj->data._current_y_out++;
 		obj->data._scaleRest -= obj->data.inputHeight;
